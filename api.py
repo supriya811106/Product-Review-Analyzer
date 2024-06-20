d8b63e8da6efab8ef393e05ecaaf50a88adb8fe4
Supriya Suman <supriya811106@gmail.com>
2024-06-20 18:22:10 +0530

    Initial commit

---
+++ api.py
@@ -0,0 +1,159 @@
+from flask import Flask, request, jsonify, send_file
+import re
+from io import BytesIO
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+import base64
+
+# Load stopwords
+STOPWORDS = set(stopwords.words("english"))
+
+app = Flask(__name__)
+
+@app.route("/test", methods=["GET"])
+def test():
+    """Endpoint to test if the service is running."""
+    return "Test request received successfully. Service is running."
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    """Endpoint to handle sentiment prediction requests."""
+    # Load models and tools from the Models folder
+    sentiment_model = pickle.load(open("Models/model_xgb.pkl", "rb"))
+    scaler = pickle.load(open("Models/scaler.pkl", "rb"))
+    vectorizer = pickle.load(open("Models/countVectorizer.pkl", "rb"))
+
+    try:
+        # Check if the request contains a file (for bulk prediction) or text input
+        if "file" in request.files:
+            # Bulk prediction from CSV file
+            uploaded_file = request.files["file"]
+            reviews_df = pd.read_csv(uploaded_file)
+
+            predictions_csv, sentiment_graph = handle_bulk_prediction(sentiment_model, scaler, vectorizer, reviews_df, request.form)
+
+            # Send the predictions and sentiment graph as response
+            response = send_file(
+                predictions_csv,
+                mimetype="text/csv",
+                as_attachment=True,
+                download_name="Predictions.csv",
+            )
+
+            response.headers["X-Graph-Exists"] = "true"
+            response.headers["X-Graph-Data"] = base64.b64encode(sentiment_graph.getbuffer()).decode("ascii")
+
+            return response
+
+        elif "text" in request.json:
+            # Single string prediction
+            review_text = request.json["text"]
+            predicted_sentiment = handle_single_prediction(sentiment_model, scaler, vectorizer, review_text, request.json)
+
+            return jsonify({"prediction": predicted_sentiment})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+def preprocess_text(text, options):
+    """Preprocess the text based on the provided options."""
+    # Convert to lowercase if option is set
+    if options.get("convert_to_lowercase", True):
+        text = text.lower()
+
+    # Remove non-alphabetic characters
+    text = re.sub("[^a-zA-Z]", " ", text)
+    text = text.split()
+
+    # Remove stopwords if option is set
+    if options.get("remove_stopwords", True):
+        text = [word for word in text if word not in STOPWORDS]
+
+    # Apply stemming if option is set
+    if options.get("use_stemming", True):
+        stemmer = PorterStemmer()
+        text = [stemmer.stem(word) for word in text]
+
+    # Apply lemmatization if option is set
+    if options.get("use_lemmatization", False):
+        lemmatizer = WordNetLemmatizer()
+        text = [lemmatizer.lemmatize(word) for word in text]
+
+    # Remove punctuation if option is set
+    if options.get("remove_punctuation", True):
+        text = [word for word in text if word.isalnum()]
+
+    return " ".join(text)
+
+def handle_single_prediction(model, scaler, vectorizer, text_input, options):
+    """Handle single text input prediction."""
+    corpus = []
+    processed_text = preprocess_text(text_input, options)
+    corpus.append(processed_text)
+    X_transformed = vectorizer.transform(corpus).toarray()
+    X_scaled = scaler.transform(X_transformed)
+    prediction_probabilities = model.predict_proba(X_scaled)
+    predicted_class = prediction_probabilities.argmax(axis=1)[0]
+
+    return "Positive" if predicted_class == 1 else "Negative"
+
+def handle_bulk_prediction(model, scaler, vectorizer, data, options):
+    """Handle bulk prediction from a DataFrame."""
+    corpus = []
+    for i in range(data.shape[0]):
+        review = data.iloc[i]["Sentence"]
+        processed_review = preprocess_text(review, options)
+        corpus.append(processed_review)
+
+    X_transformed = vectorizer.transform(corpus).toarray()
+    X_scaled = scaler.transform(X_transformed)
+    prediction_probabilities = model.predict_proba(X_scaled)
+    predicted_classes = prediction_probabilities.argmax(axis=1)
+    predicted_sentiments = list(map(map_sentiment, predicted_classes))
+
+    data["Predicted sentiment"] = predicted_sentiments
+    predictions_csv = BytesIO()
+    data.to_csv(predictions_csv, index=False)
+    predictions_csv.seek(0)
+
+    sentiment_graph = create_sentiment_distribution_graph(data)
+
+    return predictions_csv, sentiment_graph
+
+def create_sentiment_distribution_graph(data):
+    """Create a pie chart showing the distribution of sentiments."""
+    fig = plt.figure(figsize=(5, 5))
+    colors = ("green", "red")
+    wp = {"linewidth": 1, "edgecolor": "black"}
+    sentiment_counts = data["Predicted sentiment"].value_counts()
+    explode = (0.01, 0.01)
+
+    sentiment_counts.plot(
+        kind="pie",
+        autopct="%1.1f%%",
+        shadow=True,
+        colors=colors,
+        startangle=90,
+        wedgeprops=wp,
+        explode=explode,
+        title="Sentiment Distribution",
+        xlabel="",
+        ylabel="",
+    )
+
+    graph = BytesIO()
+    plt.savefig(graph, format="png")
+    plt.close()
+
+    return graph
+
+def map_sentiment(class_label):
+    """Map numeric class labels to sentiment labels."""
+    return "Positive" if class_label == 1 else "Negative"
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
---
+++ app.py
@@ -0,0 +1,163 @@
+import streamlit as st
+import pandas as pd
+import requests
+from io import BytesIO
+import base64
+import time
+
+# Apply custom CSS
+st.markdown("<style>{}</style>".format(open("static/style.css").read()), unsafe_allow_html=True)
+
+# Display logo with resized width
+st.image("static/logo.png", use_column_width=True)
+
+# Main header and description
+st.markdown(
+    "<div class='header'>"
+    "<h1>FeedbackFinder - Elevate Your Product with Customer Insights</h1>"
+    "</div>",
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    "<div class='content'>"
+    "<p>Welcome to FeedbackFinder, where you can unlock the true potential of customer feedback effortlessly. 😊</p>"
+    "<p>FeedbackFinder is a powerful and intuitive tool crafted to help you delve deep into the sentiments and opinions expressed in product reviews. "
+    "By leveraging our advanced sentiment analysis models, you can transform raw feedback into actionable insights that drive product innovation and customer satisfaction.</p>"
+    "<p class='content-text'>With FeedbackFinder, you can:</p>"
+    "<ul>"
+    "<li><b>Quickly analyze bulk reviews</b>: Upload a CSV file with multiple reviews and get sentiment predictions at scale.</li>"
+    "<li><b>Evaluate individual feedback</b>: Enter a single review to instantly understand the sentiment behind the words.</li>"
+    "<li><b>Customize your analysis</b>: Choose from various text preprocessing options to tailor the analysis to your needs.</li>"
+    "<li><b>Visualize sentiment distribution</b>: Generate and download graphs to easily interpret the overall customer sentiment.</li>"
+    "<li><b>Enhance your product strategy</b>: Use the insights gained to refine your products, address customer concerns, and celebrate what they love.</li>"
+    "</ul>"
+    "<p>Start exploring the voice of your customers today and make data-driven decisions that elevate your product offerings.</p>"
+    "</div>",
+    unsafe_allow_html=True
+)
+
+# Tabs for navigation
+tab1, tab2, tab3 = st.tabs(["Sentiment Analysis", "How It Works", "Feedback"])
+
+with tab1:
+    st.markdown("<div class='header'>"
+                "<h2>Sentiment Analysis</h2>"
+                "</div>",
+                unsafe_allow_html=True
+    )
+
+    # Model selection
+    model_option = st.selectbox(
+        "Select a model for sentiment prediction:",
+        ("XGBoost", "RandomForest", "LogisticRegression")
+    )
+
+    # File uploader and text input for prediction
+    uploaded_file = st.file_uploader("Upload a CSV file containing product reviews for bulk prediction:", type="csv")
+    user_input = st.text_area("Enter a product review for sentiment prediction:", height=150)
+
+    # Sidebar options for text preprocessing
+    st.sidebar.header("Text Preprocessing Options")
+    use_stemming = st.sidebar.checkbox("Use Stemming", value=True)
+    remove_stopwords = st.sidebar.checkbox("Remove Stopwords", value=True)
+    convert_to_lowercase = st.sidebar.checkbox("Convert to Lowercase", value=True)
+    remove_punctuation = st.sidebar.checkbox("Remove Punctuation", value=True)
+    use_lemmatization = st.sidebar.checkbox("Use Lemmatization", value=False)
+
+    # Prediction endpoint
+    prediction_endpoint = "http://127.0.0.1:5000/predict"
+
+    # Prediction button logic
+    if st.button("Predict"):
+        if uploaded_file is not None:
+            with st.spinner("Processing..."):
+                file = {"file": uploaded_file}
+                params = {
+                    "model": model_option,
+                    "use_stemming": use_stemming,
+                    "remove_stopwords": remove_stopwords,
+                    "convert_to_lowercase": convert_to_lowercase,
+                    "remove_punctuation": remove_punctuation,
+                    "use_lemmatization": use_lemmatization
+                }
+                response = requests.post(prediction_endpoint, files=file, data=params)
+                response_df = pd.read_csv(BytesIO(response.content))
+
+                st.markdown("<h3>Predictions:</h3>", unsafe_allow_html=True)
+                st.dataframe(response_df)
+
+                csv = response_df.to_csv(index=False)
+                b64 = base64.b64encode(csv.encode()).decode()
+                href = f'<a href="data:file/csv;base64,{b64}" download="Predictions.csv" class="predict-button">Download Predictions</a>'
+                st.markdown(href, unsafe_allow_html=True)
+
+                if response.headers.get('X-Graph-Exists') == 'true':
+                    graph_bytes = base64.b64decode(response.headers.get('X-Graph-Data'))
+                    st.image(graph_bytes, caption='Sentiment Distribution', use_column_width=True)
+
+        elif user_input:
+            with st.spinner("Processing..."):
+                time.sleep(1)
+                params = {
+                    "text": user_input,
+                    "model": model_option,
+                    "use_stemming": use_stemming,
+                    "remove_stopwords": remove_stopwords,
+                    "convert_to_lowercase": convert_to_lowercase,
+                    "remove_punctuation": remove_punctuation,
+                    "use_lemmatization": use_lemmatization
+                }
+                response = requests.post(prediction_endpoint, json=params).json()
+                st.markdown(f"<h3>Predicted sentiment:</h3> {response['prediction']}", unsafe_allow_html=True)
+        else:
+            st.warning("Please upload a CSV file or enter some text.")
+
+with tab2:
+    st.markdown("<div class='header'>"
+                "<h2>How It Works</h2>"
+                "</div>",
+                unsafe_allow_html=True
+    )
+    st.markdown("""
+        1. **Select a Model**: Choose a machine learning model for sentiment analysis.
+        2. **Upload Reviews**: Upload a CSV file with product reviews or enter a single review.
+        3. **Preprocess Text**: Select text preprocessing options such as stemming, stopword removal, etc.
+        4. **Predict Sentiment**: Get sentiment predictions and download the results.
+        5. **Analyze Feedback**: Use the insights to improve your products and services.
+    """)
+
+with tab3:
+    st.markdown("<div class='header'>"
+                "<h2>Feedback</h2></div>",
+                unsafe_allow_html=True
+    )
+    was_helpful = st.selectbox(
+        "Did you find our insights useful?",
+        ["Please choose an option", "Yes, very helpful", "Somewhat helpful", "Not helpful"],
+        key='feedback_select'
+    )
+    if was_helpful != "Please choose an option":
+        feedback = st.text_area("Kindly share any additional comments or suggestions...", key='feedback_text')
+        name = st.text_input("Your Name", key='feedback_name')
+        email = st.text_input("Your Email", key='feedback_email')
+        if st.button("Submit Feedback", key='feedback_button'):
+            with st.spinner("Submitting feedback..."):
+                time.sleep(2)
+                st.success("Thank you for sharing your thoughts!")
+
+# Sidebar information
+st.sidebar.markdown("---")
+st.sidebar.markdown("### About")
+st.sidebar.info(
+    "Welcome to FeedbackFinder! Our tool leverages advanced machine learning models to analyze sentiments from product reviews. "
+    "Upload a CSV file with reviews for bulk predictions, or enter a single review for real-time analysis. "
+    "Use our insights to understand customer feedback and enhance your products and services."
+)
+
+# Footer
+st.markdown("<footer>"
+            "<p>FeedbackFinder - Elevate Your Product with Customer Insights</p>"
+            "</footer>",
+            unsafe_allow_html=True
+)
